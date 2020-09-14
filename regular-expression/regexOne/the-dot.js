291c6cb468894d65fcc8ba01b2cd41f512fc32b4
Jason Kim <thehesedlife@gmail.com>
2020-09-14 10:11:36 -0700

    [regexOne] Lesson 2: The Dot

---
+++ regular-expression/regexOne/the-dot.js
@@ -0,0 +1,10 @@
+// https://regexone.com/lesson/wildcards_dot
+
+// Below are a couple strings with varying characters but the same length. Try to write a single pattern that can match the first three strings, but not the last (to be skipped). You may find that you will have to escape the dot metacharacter to match the period in some of the lines.
+
+const tasks = ["cat.", "896.", "?=+.", 'abc1'];
+const regex = /.{3}\./;
+
+tasks.forEach(element => {
+  console.log(regex.test(element))
+})
