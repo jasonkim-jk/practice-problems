4c370d96c03bfebb5f33d6ba009bf260b2309461
Jason Kim <thehesedlife@gmail.com>
2020-09-14 21:39:01 -0700

    extracting_url_data.js

---
+++ regular-expression/regexOne/extracting_url_data.js
@@ -0,0 +1,16 @@
+// https://regexone.com/problem/extracting_url_data
+
+// In the exercise below, try to extract the protocol, host and port of the all the resources listed.
+
+const tasks = [
+  "ftp://file_server.com:21/top_secret/life_changing_plans.pdf",
+  "https://regexone.com/lesson/introduction#section",
+  "file://localhost:4040/zip_file",
+  "https://s3cur3-server.com:9999/",
+  "market://search/angry%20birds"
+];
+const regex = /^(\w+):\/\/([a-z0-9_-]*[\.]?[a-z]+)[:]?(\d+)?/;
+
+tasks.forEach((element) => {
+  regex.test(element) ? console.log("Capture") : console.log("Skip");
+});
