aadef897fb94819b7a999851c90422efa74fcba0
Jason Kim <thehesedlife@gmail.com>
2020-09-16 15:21:50 -0700

    count-unique-values2.js

--- data-structure/problem-patterns/count-unique-values.js
+++ data-structure/problem-patterns/count-unique-values.js
@@ -12,4 +12,6 @@ function countUniqueValues(array) {
 console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
 console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
 console.log(countUniqueValues([]));
+console.log(countUniqueValues([1]));
+console.log(countUniqueValues([1, 1]));
 console.log(countUniqueValues([-2, -1, -1, 0, 1]));
