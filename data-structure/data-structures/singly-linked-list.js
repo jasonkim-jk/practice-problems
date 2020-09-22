7652e8b3c401079e0afe66d2c3f1ae58e7bc61ff
Jason Kim <thehesedlife@gmail.com>
2020-09-21 20:43:07 -0700

    single linked list: push

---
+++ data-structure/data-structures/singly-linked-list.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const list = new SinglyLinkedList();
+console.log(list.push("HI"));
+console.log(list.push("There"));
+console.log(list.push("good"));
+console.log(list.push("morning"));
