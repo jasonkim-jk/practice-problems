ed9827e5f69751600adfaa9414cb75176c8cc34a
Jason Kim <thehesedlife@gmail.com>
2020-09-22 17:18:54 -0700

    doubly linked list: pop

--- data-structure/data-structures/doubly-linked-list.js
+++ data-structure/data-structures/doubly-linked-list.js
@@ -16,7 +16,7 @@ class DoublyLinkedList {
   push(val) {
     const newNode = new Node(val);
 
-    if(!this.head) {
+    if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
     } else {
@@ -28,9 +28,30 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    const currentTail = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = currentTail.prev;
+      this.tail.next = null;
+      currentTail.prev = null;
+    }
+
+    this.length--;
+    return currentTail;
+  }
 }
 
 const list = new DoublyLinkedList();
 console.log(list.push(10));
 console.log(list.push(20));
 console.log(list.push(30));
+console.log(list.pop());
+console.log(list.push(40));
+console.log(list.pop());
+console.log(list.pop());
