3eb54dab46fa4e949e88ba345f29d91789586124
Jason Kim <thehesedlife@gmail.com>
2020-09-26 15:06:39 -0700

    queue: enqueue

--- data-structure/data-structures/queue.js
+++ data-structure/data-structures/queue.js
@@ -11,4 +11,29 @@ class Queue {
     this.last = null;
     this.size = 0;
   }
+
+  enqueue(val) {
+    // create a new node
+    const newNode = new Node(val);
+
+    // check if there is a node or not, if not, set the first and last to be the new node
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      // if so, set the next of the lat to be the new node and the last to be the new node
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    // increase the size and return list
+    this.size++;
+    return this;
+  }
 }
+
+const queue = new Queue();
+console.log(queue.enqueue(Math.floor(Math.random() * 100)));
+console.log(queue.enqueue(Math.floor(Math.random() * 100)));
+console.log(queue.enqueue(Math.floor(Math.random() * 100)));
+console.log(queue.enqueue(Math.floor(Math.random() * 100)));
