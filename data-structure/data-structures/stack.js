76158856ea18b1e4ed64a6fedf01fe3ce7f55cd1
Jason Kim <thehesedlife@gmail.com>
2020-09-26 12:49:20 -0700

    stack skeleton

---
+++ data-structure/data-structures/stack.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constuctor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+}
