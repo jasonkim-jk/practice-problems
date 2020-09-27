93920fcb5a6bc426be1054df24146394662a384e
Jason Kim <thehesedlife@gmail.com>
2020-09-26 22:13:52 -0700

    BinarySearchTree: find

--- data-structure/data-structures/binary-search-tree.js
+++ data-structure/data-structures/binary-search-tree.js
@@ -23,8 +23,9 @@ class BinarySearchTree {
 
     // save the root value to start at the root
     let current = this.root;
+    let stopCondition = false;
 
-    while (true) {
+    while (current && !stopCondition) {
       // check the value is same with current, if so, return undefined
       if (current.value === value) return undefined;
       // check the input value is less than or greater than the current value
@@ -32,7 +33,7 @@ class BinarySearchTree {
         // if there is no left value, that is the position of the value and return
         if (current.left === null) {
           current.left = newNode;
-          return this;
+          stopCondition = true;
         }
         // if not, save the left value to be the current to loop again
         current = current.left;
@@ -40,10 +41,43 @@ class BinarySearchTree {
         if (current.right === null) {
           // if there is no right value, that is the position of the value and return
           current.right = newNode;
-          return this;
+          stopCondition = true;
         }
         // if not, save the right value to be the current to loop again
         current = current.right;
+      }
+    }
+    return this;
+  }
+
+  find(value) {
+    // check if there is a root, if not, return false
+    if (!this.root) return false;
+
+    // save the value of the root as a current, check if it is same with the input value
+    let current = this.root;
+
+    // if it is not same, check if the value is greater than or less than the saved value
+    while (current) {
+      if (current.value === value) return true;
+      // if it is less, check if there is a left node,
+      if (value < current.value) {
+        if (current.left) {
+          // if so, save the value as the current
+          current = current.left;
+        } else {
+          // if not, return false to stop searching
+          return false;
+        }
+      } else {
+        // if it is greater, check if there is a right node,
+        if (current.right) {
+          // if so, save the value as the current
+          current = current.right;
+        } else {
+          // if not, return false to stop searching
+          return false;
+        }
       }
     }
   }
@@ -59,3 +93,12 @@ tree.insert(7);
 tree.insert(37);
 tree.insert(28);
 console.log(tree);
+console.log(tree.find(10));
+console.log(tree.find(0));
+console.log(tree.find(4));
+console.log(tree.find(3));
+console.log(tree.find(-43));
+console.log(tree.find(37));
+console.log(tree.find(50));
+console.log(tree.find(18));
+console.log(tree.find(28));
