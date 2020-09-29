399f588f9442fe5ed1750f46b9ecc85abfe0b94c
Jason Kim <thehesedlife@gmail.com>
2020-09-29 13:08:19 -0700

    [added] hash table: keys and values

--- data-structure/data-structures/hash.js
+++ data-structure/data-structures/hash.js
@@ -32,14 +32,50 @@ class HashTable {
     // check if there is any data in the index of hash table
     if (this.keyMap[index]) {
       // if so, retrieves the key-value pair in the keyMap
-      this.keyMap[index].forEach(element => {
-        if(element[0] === key) {
+      this.keyMap[index].forEach((element) => {
+        if (element[0] === key) {
           result = element[1];
         }
-      })
+      });
     }
     // if not, return undefined
     return result;
+  }
+
+  values() {
+    // declare a new array to save value data
+    let values = [];
+
+    // loop the hash table and check if there is data for each index
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          // check if a value is alredy in the array, if so, skip to save
+          if (!values.includes(this.keyMap[i][j][1])) values.push(this.keyMap[i][j][1]);
+        }
+      }
+    }
+
+    // return the array
+    return values;
+  }
+
+  keys() {
+    // declare a new array to save value data
+    let values = [];
+
+    // loop the hash table and check if there is data for each index
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          // check if a value is alredy in the array, if so, skip to save
+          if (!values.includes(this.keyMap[i][j][0])) values.push(this.keyMap[i][j][0]);
+        }
+      }
+    }
+
+    // return the array
+    return values;
   }
 
   print() {
@@ -55,7 +91,11 @@ hashTable.set("salmon", "#FA8072");
 hashTable.set("lightcoral", "#F08080");
 hashTable.set("mediumvioletred", "#C71585");
 hashTable.set("plum", "#DDA0DD");
+hashTable.set("plum", "#00000000000");
+hashTable.set("violet", "#DDA0DD");
 console.log(hashTable.print());
 console.log(hashTable.get("maroon"));
 console.log(hashTable.get("yellow"));
 console.log(hashTable.get("balck"));
+console.log(hashTable.values());
+console.log(hashTable.keys());
