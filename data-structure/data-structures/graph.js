e37e72d6be19f8baa58eef857286cb40b9583739
Jason Kim <thehesedlife@gmail.com>
2020-09-30 12:44:08 -0700

    [added] graph: addVertex

--- data-structure/data-structures/graph.js
+++ data-structure/data-structures/graph.js
@@ -2,4 +2,17 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  addVertex(name) {
+    // add a key to the adjacencyList with the name of the vertax
+    // set its value to be an empty array
+    this.adjacencyList[name] = [];
+
+    // return the adjacencyList
+    return this.adjacencyList;
+  }
 }
+
+const graph = new Graph();
+console.log(graph.addVertex("Los Angeles"));
+console.log(graph.addVertex("Las Vegas"));
