ea31fff2db6d7b29e5c3e2dcc72b1607c23b9944
Jason Kim <thehesedlife@gmail.com>
2020-10-03 01:43:28 -0700

    reverse string using array

---
+++ algocasts/reversestring-array.js
@@ -0,0 +1,9 @@
+// change the input string to an array
+// reverse the array
+// join the array to a string
+// return the string
+function reverse(str) {
+  const arr = str.split("");
+  arr.reverse();
+  return arr.join("");
+}
