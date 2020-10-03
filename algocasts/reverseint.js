abf4bcd68f927266ac76abbf3cd9da1e598ca742
Jason Kim <thehesedlife@gmail.com>
2020-10-03 13:14:51 -0700

    [updated] reverse int

--- algocasts/reverseint.js
+++ algocasts/reverseint.js
@@ -14,5 +14,5 @@
 function reverseInt(n) {
   const revStr = n.toString.split("").reverse.join("");
 
-  return n < 0 ? parseInt(revStr) * -1 : parseInt(revStr);
+  return Math.sign(n) * parseInt(revStr);
 }
