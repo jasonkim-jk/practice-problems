8119be14a0ee053e40cb66864997996f6a872d68
Jason Kim <thehesedlife@gmail.com>
2020-10-04 16:11:26 -0700

    fibonacci using recursion

--- data-structure/recursion/DP-tabulation-fibonacci.js
+++ data-structure/recursion/DP-tabulation-fibonacci.js
@@ -1,7 +1,7 @@
 function fibonacci(num) {
   if (num <= 2) return 1;
 
-  const fiboNum = [0, 1, 2];
+  const fiboNum = [0, 1, 1];
   // fiboNum[num] = fiboNum[num - 1] + fiboNum[num - 2];
   for (let i = 3; i <= num; i++) {
     fiboNum[i] = fiboNum[i - 1] + fiboNum[i - 2];
