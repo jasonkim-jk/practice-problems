4017be0b612f1d9d0aab80e42b81f4ae6f4341bf
Jason Kim <thehesedlife@gmail.com>
2020-10-04 21:22:49 -0700

    queue from stack

---
+++ algocasts/queue-from-stack.js
@@ -0,0 +1,54 @@
+// --- Directions
+// Implement a Queue datastructure using two stacks.
+// *Do not* create an array inside of the 'Queue' class.
+// Queue should implement the methods 'add', 'remove', and 'peek'.
+// For a reminder on what each method does, look back
+// at the Queue exercise.
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.add(2);
+//     q.peek();  // returns 1
+//     q.remove(); // returns 1
+//     q.remove(); // returns 2
+
+const Stack = require("./stack");
+
+class Queue {
+  constructor() {
+    this.data = new Stack();
+    this.temp = new Stack();
+  }
+
+  add(value) {
+    this.data.push(value);
+  }
+
+  remove() {
+    while (this.data.peek()) {
+      this.temp.push(this.data.pop());
+    }
+
+    const removed = this.temp.pop();
+
+    while (this.temp.peek()) {
+      this.data.push(this.temp.pop());
+    }
+
+    return removed;
+  }
+
+  peek() {
+    while (this.data.peek()) {
+      this.temp.push(this.data.pop());
+    }
+
+    const peeked = this.temp.peek();
+
+    while (this.temp.peek()) {
+      this.data.push(this.temp.pop());
+    }
+
+    return peeked;
+  }
+}
