49065e24d013286de72076c125461d8db453cb4c
Jason Kim <thehesedlife@gmail.com>
2020-10-04 17:32:46 -0700

    queue: from scratch

---
+++ algocasts/queue.js
@@ -0,0 +1,54 @@
+// --- Description
+// Create a queue data structure.  The queue
+// should be a class with methods 'add' and 'remove'.
+// Adding to the queue should store an element until
+// it is removed
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.remove(); // returns 1;
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  add(value) {
+    const newNode = new Node(value);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  remove() {
+    if (!this.first) return undefined;
+
+    const fisrtNode = this.first;
+
+    if (this.length === 1) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.length--;
+
+    return fisrtNode.value;
+  }
+}
