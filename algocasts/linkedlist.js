302a4a5c8f220e564f04c711d64a972e44c4cac3
Jason Kim <thehesedlife@gmail.com>
2020-10-08 21:09:06 -0700

    linkedList: forEach

--- algocasts/linkedlist.js
+++ algocasts/linkedlist.js
@@ -109,4 +109,23 @@ class LinkedList {
     newNode.next = prevNode.next;
     prevNode.next = newNode;
   }
+
+  forEach(fn) {
+    let node = this.head;
+    let counter = 0;
+
+    while (node) {
+      fn(node, counter);
+      node = node.next;
+      counter++;
+    }
+  }
+
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next;
+    }
+  }
 }
