86191f203014c0d68d2322f9e3c1b2ce2075fb69
Jason Kim <thehesedlife@gmail.com>
2020-10-16 11:26:27 -0700

    [updated] singly linked list: insert, traverseToIndex, printList

--- lecture2/single-linked-list.js
+++ lecture2/single-linked-list.js
@@ -15,7 +15,7 @@ class LinkList {
   append(value) {
     const newNode = new Node(value);
 
-    if(!this.head) {
+    if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
     } else {
@@ -30,7 +30,7 @@ class LinkList {
   prepend(value) {
     const newNode = new Node(value);
 
-    if(!this.head) {
+    if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
     } else {
@@ -41,6 +41,47 @@ class LinkList {
     this.length++;
     return this;
   }
+
+  insert(index, value) {
+    if (index <= 0) {
+      return this.prepend(value);
+    }
+
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    const targetNode = this.traverseToIndex(index - 1);
+    newNode.next = targetNode.next;
+    targetNode.next = newNode;
+    this.length++;
+
+    return this;
+  }
+
+  traverseToIndex(index) {
+    if(index < 0 || index > this.length - 1) return undefined;
+
+    let currentNode = this.head;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+
+    return currentNode;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+
+    while(currentNode) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    return array;
+  }
 }
 
 const myLinkedList = new LinkList();
@@ -49,3 +90,11 @@ console.log(myLinkedList.append(10));
 console.log(myLinkedList.append(15));
 console.log(myLinkedList.prepend(0));
 console.log(myLinkedList.prepend(-5));
+console.log(`size: ${myLinkedList.length}, `, myLinkedList.printList());
+console.log(myLinkedList.insert(0, -10));
+console.log(myLinkedList.insert(-3, -20));
+console.log(myLinkedList.insert(5, 20));
+console.log(myLinkedList.append(25));
+console.log(`size: ${myLinkedList.length}, `, myLinkedList.printList());
+console.log(myLinkedList.insert(1, 100));
+console.log(`size: ${myLinkedList.length}, `, myLinkedList.printList());
