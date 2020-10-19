9215e3c4d1b7eed3a400fa5ff126a6e5a6955d6f
Jason Kim <thehesedlife@gmail.com>
2020-10-19 16:21:08 -0700

    [updated] binary search tree: remove

--- lecture2/binary-search-tree.js
+++ lecture2/binary-search-tree.js
@@ -56,6 +56,67 @@ class BinarySearchTree {
 
     return false;
   }
+
+  remove(value) {
+    let currentNode = this.root;
+    let parentNode = null;
+    if (!currentNode) return false;
+
+    while (currentNode) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else if (value === currentNode.value) {
+        if (!currentNode.right) {
+          if (!parentNode) {
+            this.root = currentNode.left;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left;
+            }
+          }
+        } else if (!currentNode.right.left) {
+          if (parentNode === null) {
+            this.root = currentNode.left;
+          } else {
+            currentNode.right.left = currentNode.left;
+
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.right;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.right;
+            }
+          }
+        } else {
+          let leftmost = currentNode.right.left;
+          let leftmostParent = currentNode.right;
+          while (leftmost.left !== null) {
+            leftmostParent = leftmost;
+            leftmost = leftmost.left;
+          }
+
+          leftmostParent.left = leftmost.right;
+          leftmost.left = currentNode.left;
+          leftmost.right = currentNode.right;
+          if (parentNode === null) {
+            this.root = leftmost;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = leftmost;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = leftmost;
+            }
+          }
+        }
+        return true;
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree();
@@ -66,13 +127,15 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
-console.log(JSON.stringify(traverse(tree.root)));
 console.log(tree.lookup(20));
 console.log(tree.lookup(200));
 console.log(tree.lookup(1));
 console.log(tree.lookup());
 console.log(tree.lookup(undefined));
 console.log(tree.lookup(9));
+console.log(tree.remove(170));
+console.log(tree.remove(9));
+console.log(JSON.stringify(traverse(tree.root)));
 
 function traverse(node) {
   const tree = { value: node.value };
