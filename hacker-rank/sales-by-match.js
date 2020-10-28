432503cbbaff6ba7f282666a0d2bb1831ef19eeb
Jason Kim <thehesedlife@gmail.com>
2020-10-28 12:38:31 -0700

    [updated] sales-by-match: sockMerchant

--- hacker-rank/sales-by-match.js
+++ hacker-rank/sales-by-match.js
@@ -40,5 +40,5 @@ function sockMerchant(n, ar) {
   return pairedNum;
 }
 
-console.log(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]));
-console.log(sockMerchant(10, [1, 1, 3, 1, 2, 1, 3, 3, 3, 3]));
+console.log(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]));   // 3
+console.log(sockMerchant(10, [1, 1, 3, 1, 2, 1, 3, 3, 3, 3]));   // 4
