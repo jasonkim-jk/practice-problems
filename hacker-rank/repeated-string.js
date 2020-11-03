c80014847485fb3599dbfe50cfed5e33ac4f0297
Jason Kim <jsonhw.kim@gmail.com>
2020-11-03 10:53:01 -0800

    repeatedString

---
+++ hacker-rank/repeated-string.js
@@ -0,0 +1,32 @@
+// Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length n in the infinitely repeating string.
+
+// repeatedString has the following parameter(s):
+
+// s: a string to repeat
+// n: the number of characters to consider
+
+// Complete the repeatedString function below.
+// check if the given array has 'a' and count it
+// save the value (n / length of the given string ) * count of 'a's
+// loop though from index = 0 to n % length of the given string
+//   check if each element is 'a', then increase the total count
+// return the total count
+function repeatedString(s, n) {
+  const regex = /[a]/g;
+  const count = s.match(regex);
+
+  if (!count) return 0;
+
+  let totalCount = parseInt(n / s.length) * count.length;
+
+  for (let i = 0; i < n % s.length; i++) {
+    if (s[i] === 'a') {
+      totalCount++;
+    }
+  }
+
+  return totalCount;
+}
+
+console.log(repeatedString('aba', 10)); // 7
+console.log(repeatedString("a", 1000000000000)); // 1000000000000
